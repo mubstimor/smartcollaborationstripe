fb11058318c4cfe5ad33d5e573939fc317530902
Timothy Mubiru <mubstimor@gmail.com>
2017-03-29 19:45:52 +0100

    clean success response

--- charge2.php
+++ charge2.php
@@ -29,8 +29,6 @@ try {
         $response['message'] = "Your payment could NOT be processed because the payment system rejected the transaction. You can try again or use another card.";
     }
 
- header('Content-Type: application/json');
- echo json_encode($response);
 }
 catch(\Stripe\Error\Card $e) {
  $body = $e->getJsonBody();
