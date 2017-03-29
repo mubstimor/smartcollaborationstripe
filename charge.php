0a0e3a4ccadaa681cd8dd6602c9f1ff8a1c76d30
Timothy Mubiru <mubstimor@gmail.com>
2017-03-29 16:36:45 +0100

    change quotes

--- charge.php
+++ charge.php
@@ -3,7 +3,7 @@
 require_once('vendor/autoload.php')
 
 // set api keys
-\Stripe\Stripe::setApiKey('sk_test_sJofmAULIyYNFHMKsopEclQG');
+\Stripe\Stripe::setApiKey("sk_test_sJofmAULIyYNFHMKsopEclQG");
 
 $token = $_POST['Token'];
 $amount = $_POST['Amount'];
