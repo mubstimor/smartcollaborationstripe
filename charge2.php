8ad21117186a5595751637abd4f459a3f1094ba8
Timothy Mubiru <mubstimor@gmail.com>
2017-03-29 16:55:06 +0100

    change in token

--- charge2.php
+++ charge2.php
@@ -3,7 +3,7 @@
 require_once('vendor/stripe/stripe-php/init.php');
 
 \Stripe\Stripe::setApiKey('sk_test_sJofmAULIyYNFHMKsopEclQG');
-$charge = \Stripe\Charge::create(array('amount' => 2000, 'currency' => 'usd', 'source' => 'tok_189fqt2eZvKYlo2CTGBeg6Uq' ));
+$charge = \Stripe\Charge::create(array('amount' => 200, 'currency' => 'usd', 'source' => 'tok_1A2j7ZKfxZabGH9P2EZkOAbQ' ));
 echo $charge;
 
 
