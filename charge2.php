9bead40862a7f2aa17f226a7c88f257c92aecd80
Timothy Mubiru <mubstimor@gmail.com>
2017-03-29 17:03:45 +0100

    change implementation in charge2.php

--- charge2.php
+++ charge2.php
@@ -3,8 +3,19 @@
 require_once('vendor/stripe/stripe-php/init.php');
 
 \Stripe\Stripe::setApiKey('sk_test_sJofmAULIyYNFHMKsopEclQG');
-$charge = \Stripe\Charge::create(array('amount' => 200, 'currency' => 'usd', 'source' => 'tok_1A2j7ZKfxZabGH9P2EZkOAbQ' ));
-echo $charge;
+$charge = \Stripe\Charge::create(array('amount' => 200, 'currency' => 'gbp', 'source' => 'tok_1A2j7ZKfxZabGH9P2EZkOAbQ' ));
+
+  // Check that it was paid:
+    if ($charge->paid == true) {
+        $response = array( 'status'=> 'Success', 'message'=>'Payment has been charged!!' );
+     } else { // Charge was not paid!
+        $response = array( 'status'=> 'Failure', 'message'=>'Your payment could NOT be processed because the payment system rejected the transaction. You can try again or use another card.' );
+    }
+
+ header('Content-Type: application/json');
+ echo json_encode($response);
+
+//echo $charge;
 
 
 ?>
