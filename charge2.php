f2ba22ad32cb82510f24c65edf75d0426a7f8813
Timothy Mubiru <mubstimor@gmail.com>
2017-03-29 17:35:04 +0100

    change to imp of charge 2

--- charge2.php
+++ charge2.php
@@ -2,40 +2,50 @@
 
 require_once('vendor/stripe/stripe-php/init.php');
 
+// array for JSON response
+$response = array();
+
 \Stripe\Stripe::setApiKey('sk_test_sJofmAULIyYNFHMKsopEclQG');
 
+$token = $_POST['Token'];
+$amount = $_POST['Amount'];
+$currency = $_POST['currency'];
+$description = $_POST['description'];
+
 try {
-$charge = \Stripe\Charge::create(array('amount' => 200, 'currency' => 'gbp', 'source' => 'tok_1A2jL2KfxZabGH9PE2eVAtIj', 'description' => 'testing on ch2' ));
+$charge = \Stripe\Charge::create(array('amount' => $amount*100, 'currency' => $currency, 'source' => $token, 'description' => $description ));
 
   // Check that it was paid:
     if ($charge->paid == true) {
-        $response = array( 'status'=> 'Success', 'message'=>'Payment has been charged!!' );
+        $response['status'] = "Success";
+        $response['message'] = "Payment has been charged!!";
      } else { // Charge was not paid!
-        $response = array( 'status'=> 'Failure', 'message'=>'Your payment could NOT be processed because the payment system rejected the transaction. You can try again or use another card.' );
+        $response['Failure'] = "Failure";
+        $response['message'] = "Your payment could NOT be processed because the payment system rejected the transaction. You can try again or use another card.";
     }
 
  header('Content-Type: application/json');
  echo json_encode($response);
 }
 catch(\Stripe\Error\Card $e) {
- // The card has been declined
-header('Content-Type: application/json');
- echo json_encode($response);
+ $body = $e->getJsonBody();
+    $err  = $body['error'];
+    $response["error"] = $err['message'];
 }
 catch(\Stripe\Error\Authentication $e){
-    echo "invalid api key";
-    header('Content-Type: application/json');
- echo json_encode($response);
+    $body = $e->getJsonBody();
+    $err  = $body['error'];
+    $response["error"] = $err['message'];
 }
 catch(\Stripe\Error\InvalidRequest $e){
     $body = $e->getJsonBody();
     $err  = $body['error'];
-    echo "invalid request ".$err['message'];
-    header('Content-Type: application/json');
- echo json_encode($response);
+    $response["error"] = $err['message'];
 }
 
 
+header('Content-Type: application/json');
+echo json_encode($response);
 //echo $charge;
 
 
