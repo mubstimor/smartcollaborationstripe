a7fa10deca9f9265acbc0f0d02fa49f157ffa2e3
Timothy Mubiru <mubstimor@gmail.com>
2017-04-03 23:03:48 +0100

    send email test

--- testmail.php
+++ testmail.php
@@ -8,7 +8,7 @@ echo "use package";
 // mail
 # Instantiate the client.
 // $mgClient = new Mailgun('key-11eb04e330dfa31423e8aed18038d54c');
-// $mgClient = new Mailgun('key-11eb04e330dfa31423e8aed18038d54c', new \Http\Adapter\Guzzle6\Client());
+$mgClient = new Mailgun('key-11eb04e330dfa31423e8aed18038d54c', new \Http\Adapter\Guzzle6\Client());
 
 $mg = Mailgun::create('key-11eb04e330dfa31423e8aed18038d54c');
 # Now, compose and send your message.
@@ -19,14 +19,14 @@ $mg->message()->send('example.com', [
   'text'    => 'It is so simple to send a message.'
 ]);
 
-// $domain = "sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org";
+$domain = "sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org";
 
-// # Make the call to the client.
-// $result = $mgClient->sendMessage("$domain",
-//           array('from'    => 'Mailgun Sandbox <postmaster@sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org>',
-//                 'to'      => 'Timothy Mubiru <mubstimor@gmail.com>',
-//                 'subject' => 'Hello Timothy Mubiru',
-//                 'text'    => 'Congratulations Timothy Mubiru, you just sent an email with Mailgun!  You are truly awesome! '));
-// echo "mail sending result -> ".$result;
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+          array('from'    => 'Mailgun Sandbox <postmaster@sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org>',
+                'to'      => 'Timothy Mubiru <mubstimor@gmail.com>',
+                'subject' => 'Hello Timothy Mubiru',
+                'text'    => 'Congratulations Timothy Mubiru, you just sent an email with Mailgun!  You are truly awesome! '));
+echo "mail sending result -> ".$result;
 
 ?>
