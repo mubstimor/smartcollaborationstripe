de899837fd62ef088d5761c377d266ace0b4fb99
Timothy Mubiru <mubstimor@gmail.com>
2017-04-03 21:50:29 +0100

    test mail sending with mailgun 5 - change of mail address

--- testmail.php
+++ testmail.php
@@ -11,7 +11,7 @@ $domain = "sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org";
 # Make the call to the client.
 $result = $mgClient->sendMessage("$domain",
           array('from'    => 'Mailgun Sandbox <postmaster@sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org>',
-                'to'      => 'Timothy Mubiru <tm283@student.le.ac.uk>',
+                'to'      => 'Timothy Mubiru <mubstimor@gmail.com>',
                 'subject' => 'Hello Timothy Mubiru',
                 'text'    => 'Congratulations Timothy Mubiru, you just sent an email with Mailgun!  You are truly awesome! '));
 echo "mail sending result -> ".$result;
