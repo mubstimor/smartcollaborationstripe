b35dcbd10f21e963d594017f0d44c02f267cf345
Timothy Mubiru <mubstimor@gmail.com>
2017-04-03 22:06:39 +0100

    test msg content

--- testmail.php
+++ testmail.php
@@ -2,21 +2,31 @@
 echo "testing mail page";
 
 require 'vendor/autoload.php';
-use MailgunMailgun;
+use Mailgun\Mailgun;
 
+echo "use package";
 // mail
 # Instantiate the client.
 // $mgClient = new Mailgun('key-11eb04e330dfa31423e8aed18038d54c');
-$mgClient = new Mailgun('key-11eb04e330dfa31423e8aed18038d54c', new \Http\Adapter\Guzzle6\Client());
+// $mgClient = new Mailgun('key-11eb04e330dfa31423e8aed18038d54c', new \Http\Adapter\Guzzle6\Client());
 
-$domain = "sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org";
+$mg = Mailgun::create('key-11eb04e330dfa31423e8aed18038d54c');
+# Now, compose and send your message.
+$mg->message()->send('example.com', [
+  'from'    => 'bob@example.com', 
+  'to'      => 'mubstimor@gmail.com', 
+  'subject' => 'The PHP SDK is awesome!', 
+  'text'    => 'It is so simple to send a message.'
+]);
 
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-          array('from'    => 'Mailgun Sandbox <postmaster@sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org>',
-                'to'      => 'Timothy Mubiru <mubstimor@gmail.com>',
-                'subject' => 'Hello Timothy Mubiru',
-                'text'    => 'Congratulations Timothy Mubiru, you just sent an email with Mailgun!  You are truly awesome! '));
-echo "mail sending result -> ".$result;
+// $domain = "sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org";
+
+// # Make the call to the client.
+// $result = $mgClient->sendMessage("$domain",
+//           array('from'    => 'Mailgun Sandbox <postmaster@sandboxeb82f6b056dc4ba1b8e89c090ea35761.mailgun.org>',
+//                 'to'      => 'Timothy Mubiru <mubstimor@gmail.com>',
+//                 'subject' => 'Hello Timothy Mubiru',
+//                 'text'    => 'Congratulations Timothy Mubiru, you just sent an email with Mailgun!  You are truly awesome! '));
+// echo "mail sending result -> ".$result;
 
 ?>
