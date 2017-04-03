f699b053cae29843c5257a406eb1eae5c0e826a1
Timothy Mubiru <mubstimor@gmail.com>
2017-04-04 00:26:57 +0100

    sendgrid lib

--- testmail.php
+++ testmail.php
@@ -1,6 +1,20 @@
 <?php 
 echo "testing mail page";
 
- mail("mubstimor@gmail.com", "Stripe Hook", "hook called ");
+ //mail("mubstimor@gmail.com", "Stripe Hook", "hook called ");
+
+ // If you are not using Composer
+require("vendor/sendgrid/SendGrid.php");
+$from = new SendGrid\Email("Example User", "example@gmail.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "mubstimor@gmail.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
 ?>
