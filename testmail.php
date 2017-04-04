1508baf09a51ac94ef4d826db745cca7122e6602
Timothy Mubiru <mubstimor@gmail.com>
2017-04-04 01:31:01 +0100

    sendgrid testing 3

--- testmail.php
+++ testmail.php
@@ -7,8 +7,8 @@ error_reporting(1);
  //mail("mubstimor@gmail.com", "Stripe Hook", "hook called ");
 
  // If you are not using Composer
-// require("vendor/sendgrid/sendgrid/SendGrid.php");
-$from = new SendGrid\Email("Example User", "example@gmail.com");
+require("vendor/sendgrid/sendgrid/SendGrid.php");
+$from = new \SendGrid\Email("Example User", "example@gmail.com");
 
 echo "testing mail page ".$from;
 
