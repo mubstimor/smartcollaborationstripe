60e8e24cf5c052869a768a1e849c1fb09c0f18f1
Timothy Mubiru <mubstimor@gmail.com>
2017-04-04 01:09:34 +0100

    sendgrid testing 1

--- testmail.php
+++ testmail.php
@@ -2,13 +2,16 @@
 require 'vendor/autoload.php';
 
 error_reporting(1);
-echo "testing mail page";
+
 
  //mail("mubstimor@gmail.com", "Stripe Hook", "hook called ");
 
  // If you are not using Composer
 // require("vendor/sendgrid/sendgrid/SendGrid.php");
 $from = new SendGrid\Email("Example User", "example@gmail.com");
+
+echo "testing mail page";
+
 $subject = "Sending with SendGrid is Fun";
 $to = new SendGrid\Email("Example User", "mubstimor@gmail.com");
 $content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
