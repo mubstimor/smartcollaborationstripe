c6c6dde4e604cd6b07b7dd2193a0c68a55472014
Timothy Mubiru <mubstimor@gmail.com>
2017-04-04 16:01:50 +0100

    added a few hooks

--- processhooks.php
+++ processhooks.php
@@ -1,7 +1,8 @@
 <?php 
 
 require_once('vendor/stripe/stripe-php/init.php');
-$my_stripe_key = getenv('STRIPE_KEY');
+// $my_stripe_key = getenv('STRIPE_KEY');
+$my_stripe_key = "sk_test_sJofmAULIyYNFHMKsopEclQG";
 
 // rerieve the request's body and parse it as JSON
 $input = @file_get_contents("php://input");
@@ -9,19 +10,101 @@ $event_json = json_decode($input);
 
 \Stripe\Stripe::setApiKey($my_stripe_key);
 
+try {
+
 // Verify the event by fetching it from Stripe
 $event = \Stripe\Event::retrieve($event_json->id);
 
 // Do something with $event
  if ($event->id == true) {
     $event_type = $event->type;
+    $customer = \Stripe\Customer::retrieve($event->data->object->customer);
+    $email = $customer->email;
+
     if($event_type == "customer.subscription.created"){
-         mail("mubstimor@gmail.com", "Stripe Hook", "subscription for client created");
-    }else{
-        mail("mubstimor@gmail.com", "Stripe Hook", "hook ".$event_type." called ");
+         mail($email, "Stripe Hook", "subscription for client created");
     }
+    else if($event_type == "invoice.payment_failed"){
+        // convert amount to pounds
+         $amount = sprintf('%0.2f', $event->data->object->amount_due / 100.0);
+        notifyAppOnTransaction($customer->id, $amount, 'pending');
+        mail($email, "Payment for subscription unsuccessful", "Transaction for ".$amount."failed");
+    }
+    else if($event_type == "invoice.payment_succeeded"){
+        $amount = sprintf('%0.2f', $event->data->object->amount_due / 100.0);
+        notifyAppOnTransaction($customer->id, $amount, 'active');
+        mail($email, "Payment Received", "Transaction for ".$amount."successful");
+    }
+    else if($event_type == "customer.subscription.updated"){
+        // billing period updated
+
+    }
+    else if($event_type == "customer.subscription.deleted"){
+        $amount = sprintf('%0.2f', $event->data->object->amount_due / 100.0);
+        notifyAppOnTransaction($customer->id, $amount, 'deactivated');
+        mail($email, "Subscription ended", "Subscription for account has been deactivated.");
+    }
+
  }
 
 http_response_code(200); 
+
+}
+catch(\Stripe\Error\Card $e) {
+ $body = $e->getJsonBody();
+    $err  = $body['error'];
+    echo $err['message'];
+}
+catch(\Stripe\Error\Authentication $e){
+    $body = $e->getJsonBody();
+    $err  = $body['error'];
+    echo $err['message'];
+}
+catch(\Stripe\Error\InvalidRequest $e){
+    $body = $e->getJsonBody();
+    $err  = $body['error'];
+    echo $err['message'];
+}
+catch(\Stripe\Error\Base $e){
+    echo "unable to process payment";
+}
+
+function notifyAppOnTransaction($customer, $amount, $status){
+// Your ID and token
+$blogID = '8070105920543249955';
+$authToken = 'xzcdsfrfawskfesd';
+
+// The data to send to the API
+$postData = array(
+    'customer_id' => $customer,
+    'amount' => $amount,
+    'status' => $status
+);
+
+// Create the context for the request
+$context = stream_context_create(array(
+    'http' => array(
+        // http://www.php.net/manual/en/context.http.php
+        'method' => 'POST',
+        'header' => "Authorization: {$authToken}\r\n".
+            "Content-Type: application/json\r\n",
+        'content' => json_encode($postData)
+    )
+));
+
+// Send the request
+$response = file_get_contents('http://smartcollaborationvapor.herokuapp.com/paymentupdates', FALSE, $context);
+
+// Check for errors
+if($response === FALSE){
+    die('Error');
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+
+// Print the date from the response
+return $responseData['recorded'];
+}
 
 ?>
--- testmail.php
+++ testmail.php
@@ -1,28 +1,11 @@
 <?php 
-require 'vendor/autoload.php';
 
 error_reporting(1);
 
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-echo $sg;
- //mail("mubstimor@gmail.com", "Stripe Hook", "hook called ");
-
- // If you are not using Composer
-require("vendor/sendgrid/sendgrid/SendGrid.php");
-$from = new \SendGrid\Email("Example User", "example@gmail.com");
-
-echo "testing mail page ".$from;
-
-$subject = "Sending with SendGrid is Fun";
-$to = new \SendGrid\Email("Example User", "mubstimor@gmail.com");
-$content = new \SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
-$mail = new \SendGrid\Mail($from, $subject, $to, $content);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+ if(mail("mubstimor@gmail.com", "Stripe Hook", "hook called ")){
+     echo "mail sent";
+ }else{
+     echo "mail not sent";
+ }
 
 ?>
