ecb994e4ecc91d5a9f1d543bcad8ea33bafc328e
Timothy Mubiru <mubstimor@gmail.com>
2017-04-04 18:12:41 +0100

    get invoice data

--- getinvoice.php
+++ getinvoice.php
@@ -4,8 +4,14 @@ $my_stripe_key = getenv('STRIPE_KEY');
 
 \Stripe\Stripe::setApiKey($my_stripe_key);
 
-\Stripe\Invoice::create(array(
-    "customer" => "cus_APk7l3Gz4Hk5QY"
-));
+$invoice = \Stripe\Invoice::create(array("customer" => "cus_APk7l3Gz4Hk5QY"));
+
+if ($invoice->id == true) {
+        //$response['status'] = "Success";
+        // $response['message'] = "Payment has been charged!!";
+        echo "amount due ".$invoice->amount_due;
+     }else{
+        echo "invoice not created";
+     }
 
  ?>
